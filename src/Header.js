46848a53726b278d6485d97dd945c1d7d87ecd2f
Chamon562 <Chamon562@gmail.com>
2020-12-07 22:18:30 -0800

    [Header.js & Header.css] Add img tag with tinder logo, resized tinder icon in Header.css along with placement and border-bottom, and deleted the h2 tag from Header.js

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,14 @@ import IconButton from '@material-ui/core/IconButton';
 function Header() {
     return (
         <div className="header">
-            <h2>I am Header</h2>
             <IconButton>
                 <AccountBoxIcon fontSize="large" className="header_icon" />
             </IconButton>
+
+            <img className="header_logo"
+                src="https://1000Logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt=""
+            />
         </div>
     );
 }
