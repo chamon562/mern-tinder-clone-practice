1c7a66d21a8c2e5469d4d1c19fffc44227cc1628
Chamon562 <Chamon562@gmail.com>
2020-12-07 22:03:58 -0800

    [Header.js] Add ripple effect when clicking AccountBoxIcon by import IconButton and Wrap IconButton around AccountBoxIcon

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <div className="header">
             <h2>I am Header</h2>
-            <AccountBoxIcon fontSize="large" className="header_icon"/>
+            <IconButton>
+                <AccountBoxIcon fontSize="large" className="header_icon" />
+            </IconButton>
         </div>
-    )
+    );
 }
 
 export default Header;
