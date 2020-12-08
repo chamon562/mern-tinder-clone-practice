b83e394f0418c24fa0f7ef20f9f2da9477f3fba1
Chamon562 <Chamon562@gmail.com>
2020-12-07 22:00:13 -0800

    [Heder.js] Install material-ui core and icon package and add AccountBoxIcon and import with fontSize to large

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <h1>Lets build the MERN TINDER clone!</h1>
       
     <Header />
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 function Header() {
     return (
         <div className="header">
             <h2>I am Header</h2>
+            <AccountBoxIcon fontSize="large" className="header_icon"/>
         </div>
     )
 }
