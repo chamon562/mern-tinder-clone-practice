3d6663c75eeebd7e11102a912d4209133cd443f3
Chamon562 <Chamon562@gmail.com>
2020-12-07 21:39:53 -0800

    [Header.js] Fix Header.js and Header.css component to show properly on localhost

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header"
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <h1>Lets build the MERN TINDER clone!</h1>
-    
-    {/* Header */}
-    {/* TinderCards */}
-    {/* SwipeButtons */}
+      
+    <Header />
     </div>
   );
 }
