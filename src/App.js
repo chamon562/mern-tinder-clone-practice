952d41c6d40d69c5980cbebea2c278222bb62185
Chamon562 <Chamon562@gmail.com>
2020-12-08 17:45:21 -0800

    [SwipeButtons.js] Add className swipeButtons inside div tag

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import "./App.css";
 import Header from "./Header"
 import TinderCards from "./TinderCards";
 import SwipeButtons from "./SwipeButtons";
+
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       {/* Header */}
       <Header />
-      
+
       {/* TinderCards */}
       <TinderCards />
-      
+
       {/* SwipeButtons */}
       <SwipeButtons />
     </div>
--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./SwipeButtons.css"
 
 function SwipeButtons(){
     return(
-        <div>
+        <div className="swipeButtons">
 
         </div>
     )
