8eefbc4aa575d65eda8fc3bc87b9bda535ed9c02
Chamon562 <Chamon562@gmail.com>
2020-12-08 17:42:12 -0800

    [App.js] Import SwipeButtons and create a SwipeButton.js and SwipeButtons.css, and hashed out basic function set up for Swipebuttons.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./Header"
 import TinderCards from "./TinderCards";
-
+import SwipeButtons from "./SwipeButtons";
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
+      {/* Header */}
+      <Header />
       
-    <Header />
-    {/* TinderCards */}
-    <TinderCards />
-    {/* SwipeButtons */}
+      {/* TinderCards */}
+      <TinderCards />
+      
+      {/* SwipeButtons */}
+      <SwipeButtons />
     </div>
   );
 }
