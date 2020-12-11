180039b2b9fefad1e080c5c47e7c7952f1786946
Chamon562 <Chamon562@gmail.com>
2020-12-11 13:06:17 -0800

    [axios.js] comment out baseURL for my reference on what it was to what will be now pasting in heroku backend url and took out the forward slash at the ned of the url.

--- cm-mern-tinder-clone-frontend/src/axios.js
+++ cm-mern-tinder-clone-frontend/src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 // create an instance 
 const instance = axios.create({
     // add the parameters which will be http://localhost:8000
-    baseURL: "http://localhost:8000"
+    // baseURL: "http://localhost:8000"
+    baseURL: "https://cm-tinder-clone-backend.herokuapp.com"
 });
 
 export default instance;
