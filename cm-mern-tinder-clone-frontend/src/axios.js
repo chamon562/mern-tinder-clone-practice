56cd2ddf0ccc1f44f9fbf81a2483110a3d7a8519
Chamon562 <Chamon562@gmail.com>
2020-12-11 10:45:45 -0800

    [axios.js] Add instance equal axios.create open up paranthesis and curly brackets inside add baseURL to http://localhost:8000 and then export default instance.

--- cm-mern-tinder-clone-frontend/src/axios.js
+++ cm-mern-tinder-clone-frontend/src/axios.js
@@ -1,1 +1,9 @@
 import axios from "axios"
+
+// create an instance 
+const instance = axios.create({
+    // add the parameters which will be http://localhost:8000
+    baseUrl: "http://localhost:8000"
+});
+
+export default instance
